cd78770a4ada7fdcf4e663c5e0fc37e08fa81c4d
José Miguel <jm.suarez201@uniandes.edu.co>
2016-09-28 17:04:29 -0500

    lti: corrección de error por inexistencia de parámetros

--- routes/index.js
+++ routes/index.js
@@ -109,6 +109,9 @@ router.post('/', function(req, res) {
 });
 router.post('/access', function(req, res) {
     console.log("Coursera response 2 POST:/access/", req.body);
+    var curso = "1";
+    var nivel = "2";
+    var examen = "3";
     res.redirect("https://accounts.coursera.org/oauth2/v1/auth?response_type=code&client_id=fFH0i9s6B-a27m5_vw48kA&redirect_uri=http%3A%2F%2Fwww.cupiexamenes.com%2Flti&scope=view_profile&state=" + curso + "-" + nivel + "-" + examen);
 });
 router.post('/lti/callback', function(req, res) {
