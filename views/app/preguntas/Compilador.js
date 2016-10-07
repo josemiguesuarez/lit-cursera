a8735dcbd12f63984c6dd20d69223462c5bc95e4
José Miguel <jm.suarez201@uniandes.edu.co>
2016-10-07 17:22:16 -0500

    Código inicial para llamar al servicio de compilación

---
+++ views/app/preguntas/Compilador.js
@@ -0,0 +1,26 @@
+(function() {
+    angular.module('app').service('Compilador', function(Http) {
+        var self = this;
+        self.compilar = function(code, array) {
+            return Http.post('compilador/compilar', {
+                code: code
+            }).then(function(data) {
+                console.log(data);
+                function pushNewLines( newStr, tipo) {
+                    var newArr = (newStr) ? newStr.split(new RegExp('\r?\n', 'g')) : [];
+                    for (var i = 0; i < newArr.length; i++) {
+                        array.push({
+                            linea: newArr[i],
+                            tipo: tipo
+                        });
+                    }
+                }
+                pushNewLines( data.stdout, 'stdout');
+                pushNewLines( data.stderr, 'stderr');
+                pushNewLines( data.err, 'err');
+                console.log("array:", array)
+            });
+
+        };
+    });
+})();
--- views/app/preguntas/preguntas.js
+++ views/app/preguntas/preguntas.js
@@ -1,16 +1,16 @@
 (function() {
-    angular.module('app').controller('PreguntasCtrl', function ($scope, $mdDialog, Pregunta) {
-        $scope.pregunta={
-            "id":1,
-            "numero":1,
-            "enunciado":"Esto es un mock del enunciado de una pregunta",
-            "porcentaje":20,
-            "esqueleto":"esto es codigo esqueleto"
+    angular.module('app').controller('PreguntasCtrl', function($scope, $mdDialog, Pregunta, Compilador) {
+        $scope.pregunta = {
+            "id": 1,
+            "numero": 1,
+            "enunciado": "Esto es un mock del enunciado de una pregunta",
+            "porcentaje": 20,
+            "esqueleto": "esto es codigo esqueleto"
         };
         $scope.imagen = "https://users.dcc.uchile.cl/~psalinas/uml/img/modelo/herencia2.jpg";
-        $scope.numero=['1','2','3','4'];
-        $scope.respuesta=angular.copy($scope.pregunta.esqueleto);
-        $scope.showImage=false;
+        $scope.numero = ['1', '2', '3', '4'];
+        $scope.respuesta = angular.copy($scope.pregunta.esqueleto);
+        $scope.showImage = false;
         $scope.undoRespuesta = function(ev) {
             var confirm = $mdDialog.confirm()
                 .title('¿Quieres deshacer tu código?')
@@ -20,20 +20,30 @@
                 .ok('Quiero borrarlo')
                 .cancel('Volver');
             $mdDialog.show(confirm).then(function() {
-                $scope.respuesta=angular.copy($scope.pregunta.esqueleto);
-            }, function() {
-            });
+                $scope.respuesta = angular.copy($scope.pregunta.esqueleto);
+            }, function() {});
         };
 
-        $scope.enviar = function(){
+        $scope.enviar = function() {
             var respuesta = {
                 texto: $scope.respuesta,
                 preguntaId: $scope.pregunta.id
             };
             console.log(respuesta);
-            Pregunta.guardarRespuesta(respuesta).then(function(data){
+            Pregunta.guardarRespuesta(respuesta).then(function(data) {
                 console.log(data);
             });
+        };
+        $scope.compilar = function() {
+            $scope.compiling = true;
+            var codigo = $scope.respuesta;
+
+            Compilador.compilar(codigo, $scope.consoleLog).then(function(){
+              $scope.compiling = false;
+            }, function(){
+              $scope.compiling = false;
+            });
+
         };
     });
 })();
